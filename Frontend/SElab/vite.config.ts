dec0f67329e885e4894f546f085132a3cff36b7c
notgMelon <fqr2022@mail.ustc.edu.cn>
2025-11-24 10:38:42 +0800

    前端代理后端服务器地址配置

--- Frontend/SElab/vite.config.ts
+++ Frontend/SElab/vite.config.ts
@@ -1,27 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig( ({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    proxy: {
-      // 当MSW禁用时，将API请求代理到真实后端
-      '/api': {
-        target: process.env.VITE_APP_BACKEND_URL,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
+    server: {
+      proxy: {
+        // 当MSW禁用时，将API请求代理到真实后端
+        '/api': {
+          target: env.VITE_APP_BACKEND_URL,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
