317d4822a11415f691023dd22c1785799ff2814f
Procrastinatorrrr <18924614955@163.com>
2025-11-24 16:14:27 +0800

    加入ON DELETE CASCADE

--- databasepart/table_build.sql
+++ databasepart/table_build.sql
@@ -1,3 +1,4 @@
+
 drop DATABASE soft_ware_engineering;
 create DATABASE if NOT EXISTS soft_ware_engineering;
 use soft_ware_engineering;
@@ -6,6 +7,7 @@ drop table if EXISTS user;
 drop Table if EXISTS movie_genre;
 drop TABLE if exists genre_table;
 drop table if exists movie;
+DROP TABLE IF EXISTS item_similarities;
 CREATE table if NOT EXISTS user(
     user_id int primary key auto_increment,
     user_name VARCHAR(50),
@@ -23,8 +25,8 @@ CREATE TABLE if NOT exists user_judge(
     movie_id int,
     rating DECIMAL(2,1),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  # 添加时间戳字段
-    Foreign Key (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    Foreign Key (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
     PRIMARY KEY(user_id, movie_id),
     check (rating > 0 AND rating <= 5.0 AND (rating * 2) = FLOOR(rating * 2)) # 小数部分只能是0或5, 0.5-5
 ); # check
@@ -38,8 +40,8 @@ create Table if not exists genre_table(
 create Table if not exists movie_genre(# 影片的类别
     movie_id int,
     genre_id SMALLINT,
-    Foreign Key (movie_id) REFERENCES movie(movie_id),
-    Foreign Key (genre_id) REFERENCES genre_table(genre_id),
+    Foreign Key (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    Foreign Key (genre_id) REFERENCES genre_table(genre_id) ON DELETE CASCADE,
     PRIMARY key(movie_id, genre_id)
 ); # check and done
 
@@ -48,18 +50,19 @@ create table if not exists user_comment(
     movie_id int,
     comment VARCHAR(1000),
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  # 添加时间戳字段
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
     PRIMARY KEY(user_id, movie_id)
 ); # check and done
 
 # 以下为推荐系统使用
-DROP TABLE IF EXISTS item_similarities;
 -- 创建 item_similarities 表（用于存 Top-K 相似物品）
 CREATE TABLE IF NOT EXISTS item_similarities (
     movie_id INT NOT NULL,
     similar_movie_id INT NOT NULL,
     similarity DECIMAL(5,4) NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
+    FOREIGN KEY (similar_movie_id) REFERENCES movie(movie_id) ON DELETE CASCADE,
     PRIMARY KEY (movie_id, similar_movie_id),
     INDEX idx_movie_id (movie_id),
     INDEX idx_similarity (similarity DESC)
